2d48f93c2b0620c3836e0fc322e01df48dc5744b
freecracy <12114073+freecracy@users.noreply.github.com>
2019-11-24 23:17:55 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
 set -e
-
-#npm run build
+rm -rf articles/.vuepress/dist/*
+yarn run build
 
 cd articles/.vuepress/dist
 
 echo 'www.cn100800.tk' > CNAME
 
 git init
+git config user.name freecracy
+git config user.email 12114073+freecracy@users.noreply.github.com
 git add -A
 git commit -m 'deploy'
 
