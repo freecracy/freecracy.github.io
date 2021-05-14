35dc65e473da1fb21d6e68432c60263759b59e96
freecracy <12114073+freecracy@users.noreply.github.com>
2021-05-15 02:07:33 +0800

    v0.0.2

--- docs/.vuepress/.config.js
+++ docs/.vuepress/.config.js
@@ -13,10 +13,10 @@ module.exports = {
       { text: "上传", link: "https://upload.cn100800.tk" },
       { text: "下载", link: "https://download.cn100800.tk" },
       { text: "联系我", link: "mailto:freecracy1024@gmail.com" },
-      { text: "google", link: "https://google.com.hk" },
+      { text: "google", link: "https://google.com.hk" }
     ],
     lastUpdated: "最后更新",
-    sidebar,
+    sidebar
   },
   plugins: [
     [
@@ -30,26 +30,27 @@ module.exports = {
       {
         selector: "img.zoom-custom-imgs",
         options: {
-          margin: 16,
-        },
-      },
+          margin: 16
+        }
+      }
     ],
     [
       "@vuepress/google-analytics",
       {
-        ga: "UA-129359790-1",
-      },
+        ga: "UA-129359790-1"
+      }
     ],
     [
       "qrcode",
       {
         labelText: "Mobile Read",
-        size: "small",
-      },
-    ],
+        size: "small"
+      }
+    ]
   ],
   head: [["meta", { name: "referrer", content: "no-referrer" }]],
   markdown: {
-    lineNumbers: true,
+    lineNumbers: true
   },
+  dest: "dist"
 };
